35797eebddee6f774db999f71f5340d95d0154da
Yan Santos <yansantos.dev@gmail.com>
2022-07-11 15:57:07 -0300

    Implementando Singleton Pattern
    Nessa etapa, o método "getInstance" fica responsável por chamar o construtor, tornando o construtor privado

--- src/modules/cars/useCases/ListCategories/index.ts
+++ src/modules/cars/useCases/ListCategories/index.ts
@@ -2,10 +2,12 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 
-const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
+const listCategoriesController = new ListCategoriesController(
+  listCategoriesUseCase
+);
 
-export {listCategoriesController}
+export { listCategoriesController };
--- ts.ts
+++
@@ -1,2 +0,0 @@
-
-let teste = {reste: 'dd'}
