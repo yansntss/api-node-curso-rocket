cb21194efce8c9b27a3e866a84c3c7ff19700e4e
Yan Santos <yansantos.dev@gmail.com>
2022-07-13 20:10:46 -0300

    refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
-import express from 'express';
+import express from "express";
+import { router } from "./routes";
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specification.routes';
- 
 const app = express();
 
-app.use(express.json())
-app.use("/categories",categoriesRoutes)
-app.use("/specifications",specificationRoutes)
+app.use(express.json());
 
-
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
-
-
-
-
