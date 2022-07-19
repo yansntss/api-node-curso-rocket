0379cf0695f04e10984cb950281c940d172916d0
Yan Santos <yansantos.dev@gmail.com>
2022-07-19 06:44:08 -0300

    Criando use case de importação de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,24 @@
-import {Router} from 'express'
+import { Router } from "express";
+import multer from "multer";
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { listCategoriesController } from '../modules/cars/useCases/ListCategories';
-
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
+const upload = multer({ dest: "./tmp" });
+
 categoriesRoutes.post("/", (request, response) => {
- return createCategoryController.handle(request, response)
-})
+  return createCategoryController.handle(request, response);
+});
 
 categoriesRoutes.get("/", (request, response) => {
-return listCategoriesController.handle(request, response);
-})
+  return listCategoriesController.handle(request, response);
+});
 
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
+});
 
-export { categoriesRoutes}
+export { categoriesRoutes };
